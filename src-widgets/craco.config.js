0cfbf65c44f5f3540de5e130eb3cd6629e356ae0
steiger04 <steiger04@posteo.de>
2024-06-30 12:19:39 +0200

    Dependencies updated

--- src-widgets/craco.config.js
+++ src-widgets/craco.config.js
@@ -1,23 +1,19 @@
 const craco = require("@iobroker/vis-2-widgets-react-dev/craco.config.js");
+// const { ModuleFederationPlugin } = require('webpack').container;
 
 module.exports = {
-    ...craco,
-    ...{
-        reactScriptsVersion: "react-scripts",
-        webpack: {
-            mode: "extends",
-            configure: {
-                module: {
-                    rules: [
-                        {
-                            test: /\.js$/,
-                            enforce: "pre",
-                            use: ["source-map-loader"],
-                        },
-                    ],
-                },
-                ignoreWarnings: [/Failed to parse source map/],
-            },
-        },
+    ...craco,    
+    reactScriptsVersion: "react-scripts",
+    webpack: {            
+        mode: "extends",
+        configure: (webpackConfig) => {            
+            webpackConfig.module.rules.push({
+                test: /\.js$/,
+                enforce: "pre",
+                use: ["source-map-loader"],
+            });
+            webpackConfig.ignoreWarnings = [/Failed to parse source map/];
+            return webpackConfig;
+        },  
     },
 };
